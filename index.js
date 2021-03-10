be7721bd3569a9a20aef34945b8704af188d985a
Rodrigo <istikis@gmail.com>
2021-03-10 13:33:25 +0100

    establece ip en cero para heroku

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // Arrancar la app
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
